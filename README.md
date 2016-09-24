b7c4d4bc7a6b3eeb794f74bcc4fe657bbe987fdf
chulanovskyi <skyrocker@mail.ru>
2016-09-25 01:15:44 +0300

    correction after remarks

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # 3_bars
+
+Prerequisites:
+Scripts need a list of bars in JSON format. Just copy the file 'Bars.json' to the directory with the script.
+List of bars can be downloaded directly from http://data.mos.ru/opendata/export/1796/json/2/1
+
+This script can help you to determine 3 things:
+
+- Biggest bar
+- Smallest bar
+- Closest to you bar
+
+How to use:
+
+All functions take one argument (data) - a list of bars in JSON format and returns bar (name, address).
+
+get_biggest_bar(data) - bar with the biggest number of seats.
+get_smallest_bar(data) - bar with the smallest number of seats.
+get_closest_bar(data) -  bar which is closest to your location. Your location needs to be entered manually.
+
--- bars.py
+++ bars.py
@@ -1,59 +1,76 @@
 import json
+import operator
 
 
 def load_data(filepath):
     with open(filepath, encoding='utf8') as file:
-        json_file = json.loads(file.read())
+        json_file = json.load(file)
     return json_file
         
 
 def get_biggest_bar(data):
-    biggest_bar = dict()
-    for bar in data:
-        if biggest_bar:
-            if bar['Cells']['SeatsCount'] > biggest_bar['Cells']['SeatsCount']:
-                biggest_bar.clear()
-                biggest_bar.update(bar)
-        else:
-            biggest_bar.update(bar)
-    print('Biggest bar is:\n{bar}'.format(bar=biggest_bar['Cells']['Name']))
-    print(biggest_bar['Cells']['Address']+'\n')
+    bars_ids_and_seats = {bar['Id']: bar['Cells']['SeatsCount'] for bar in data}
+    max_seats = max(bars_ids_and_seats.items(), key=operator.itemgetter(1))
+    biggest_bar = [bar for bar in data if bar['Id'] == max_seats[0]][0]
     return biggest_bar
 
 
 def get_smallest_bar(data):
-    smallest_bar = dict()
-    for bar in data:
-        if smallest_bar:
-            if bar['Cells']['SeatsCount'] < smallest_bar['Cells']['SeatsCount']:
-                smallest_bar.clear()
-                smallest_bar.update(bar)
-        else:
-            smallest_bar.update(bar)
-    print('Smallest bar is:\n{bar}'.format(bar=smallest_bar['Cells']['Name']))
-    print(smallest_bar['Cells']['Address']+'\n')
+    bars_ids_and_seats = {bar['Id']: bar['Cells']['SeatsCount'] for bar in data}
+    min_seats = min(bars_ids_and_seats.items(), key=operator.itemgetter(1))
+    smallest_bar = [bar for bar in data if bar['Id'] == min_seats[0]][0]
     return smallest_bar
 
 
-def get_closest_bar(data, longitude, latitude):
+def get_closest_bar(data):
+    user_location = get_user_coordinates()
+    if not user_location:
+        return
     closest_bar = dict()
     for bar in data:
         if closest_bar:
-            bar_coords = bar['Cells']['geoData']['coordinates']
-            closest_coords = closest_bar['Cells']['geoData']['coordinates']
-            bar_delta = sum([abs(bar_coords[0] - longitude), abs(bar_coords[1] - latitude)])
-            closest_delta = sum([abs(closest_coords[0] - longitude), abs(closest_coords[1] - latitude)])
-            if bar_delta < closest_delta:
+            bar_location = bar['Cells']['geoData']['coordinates']
+            closest_bar_location = closest_bar['Cells']['geoData']['coordinates']
+            bar_distance = find_distance_between_points(bar_location, user_location)
+            closest_bar_distance = find_distance_between_points(closest_bar_location, user_location)
+            if bar_distance < closest_bar_distance:
                 closest_bar.clear()
                 closest_bar.update(bar)
         else:
             closest_bar.update(bar)
-    print('Closest bar to you is:\n%s' % closest_bar['Cells']['Name'])
-    print(closest_bar['Cells']['Address'])
+    
     return closest_bar
+
+
+def get_user_coordinates():
+    longitude = input('Enter longitude: ')
+    latitude = input('Enter latitude: ')
+    try:
+        coordinates = (float(longitude),float(latitude))
+        return coordinates
+    except ValueError:
+        print('Enter digits only')
+    
+
+
+def find_distance_between_points(point_1, point_2):
+    '''
+    point_1 & point_2 supposed to be tuples, that consists of two float numbers.
+    '''
+    return sum([abs(point_1[0] - point_2[0]), abs(point_1[1] - point_2[1])])
+
+
+def print_bar_info(bar, head=None):
+    if head:
+        print(head)
+    print('Name: %s' % bar['Cells']['Name'])
+    print('Address: %s' % bar['Cells']['Address'])
+    print('Location: %s\n' % bar['Cells']['geoData']['coordinates'])
+
 
 if __name__ == '__main__':
     data = load_data('Bars.json')
-    get_biggest_bar(data)
-    get_smallest_bar(data)
-    get_closest_bar(data, 37.502862999552, 55.742721000053)
+
+    print_bar_info(get_biggest_bar(data), 'Biggest bar is:')
+    print_bar_info(get_smallest_bar(data), 'Smallest bar is')
+    print_bar_info(get_closest_bar(data), 'Closest bar to you is:')
