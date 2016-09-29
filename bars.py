c346801a438ce54ddf12d8ce3a826ebcf129ba69
chulanovskyi <skyrocker@mail.ru>
2016-09-29 14:20:24 +0300

    a little shorter

--- bars.py
+++ bars.py
@@ -10,15 +10,15 @@ def load_data(filepath):
 
 def get_biggest_bar(data):
     bars_ids_and_seats = {bar['Id']: bar['Cells']['SeatsCount'] for bar in data}
-    max_seats = max(bars_ids_and_seats.items(), key=operator.itemgetter(1))
-    biggest_bar = [bar for bar in data if bar['Id'] == max_seats[0]][0]
+    bar_id_by_max_seats = max(bars_ids_and_seats.items(), key=operator.itemgetter(1))[0]
+    biggest_bar = next(bar for bar in data if bar['Id'] == bar_id_by_max_seats)
     return biggest_bar
 
 
 def get_smallest_bar(data):
     bars_ids_and_seats = {bar['Id']: bar['Cells']['SeatsCount'] for bar in data}
-    min_seats = min(bars_ids_and_seats.items(), key=operator.itemgetter(1))
-    smallest_bar = [bar for bar in data if bar['Id'] == min_seats[0]][0]
+    bar_id_by_min_seats = min(bars_ids_and_seats.items(), key=operator.itemgetter(1))[0]
+    smallest_bar = next(bar for bar in data if bar['Id'] == bar_id_by_min_seats)
     return smallest_bar
 
 
@@ -34,11 +34,9 @@ def get_closest_bar(data):
             bar_distance = find_distance_between_points(bar_location, user_location)
             closest_bar_distance = find_distance_between_points(closest_bar_location, user_location)
             if bar_distance < closest_bar_distance:
-                closest_bar.clear()
-                closest_bar.update(bar)
+                closest_bar = bar
         else:
-            closest_bar.update(bar)
-    
+            closest_bar = bar
     return closest_bar
 
 
@@ -50,23 +48,21 @@ def get_user_coordinates():
         return coordinates
     except ValueError:
         print('Enter digits only')
-    
 
 
 def find_distance_between_points(point_1, point_2):
-    point_1_x = point_1[0]
-    point_1_y = point_1[1]
-    point_2_x = point_2[0]
-    point_2_y = point_2[1]
+    point_1_x, point_1_y = point_1
+    point_2_x, point_2_y = point_2
     return sum([abs(point_1_x - point_2_x), abs(point_1_y - point_2_y)])
 
 
 def print_bar_info(bar, head=None):
     if head:
         print(head)
-    print('Name: %s' % bar['Cells']['Name'])
-    print('Address: %s' % bar['Cells']['Address'])
-    print('Location: %s\n' % bar['Cells']['geoData']['coordinates'])
+    if bar:
+        print('Name: %s' % bar['Cells']['Name'])
+        print('Address: %s' % bar['Cells']['Address'])
+        print('Location: %s\n' % bar['Cells']['geoData']['coordinates'])
 
 
 if __name__ == '__main__':
